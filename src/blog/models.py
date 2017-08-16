ba04d5d34fb4b01553ea0a1c6e0fed98c1a15933
Luis Carlos Mencia <lcmencia2@gmail.com>
2017-08-16 16:43:03 -0400

    Creating list and detail views

--- src/blog/models.py
+++ src/blog/models.py
@@ -1,23 +1,35 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User 
+from django.template.defaultfilters import slugify
 
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager,self).get_queryset().filter(status='published')
 class Post(models.Model):
     STATUS_CHOICES = (
         ('draft', 'Draft'),
         ('published', 'Published'),
     )
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250, unique_for_date='publish')
+    slug = models.SlugField(max_length=250, unique_for_date='publish', editable=False)
     author = models.ForeignKey(User, related_name='blog_posts')
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+    objects = models.Manager()
+    published = PublishedManager()
 
     class Meta:
         ordering = ('-publish',)
     
+
     def __str__(self):
         return self.title
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.slug = slugify(self.title)
+        super(Post, self).save(*args, **kwargs)
