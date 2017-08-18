1c7c0e2bd6bbf0e371bc4e09483cb68a187751e1
Luis Carlos Mencia <lcmencia2@gmail.com>
2017-08-18 13:21:32 -0400

    no message

--- src/blog/views.py
+++ src/blog/views.py
@@ -3,25 +3,13 @@ from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 
+
+
 class PostListView(ListView):
     queryset = Post.published.all()
     context_object_name = 'posts'
     paginate_by = 3 
     template_name = 'blog/post/list.html'
-
-# def post_list(request):
-#     object_list = Post.published.all()
-#     paginator = Paginator(object_list, 3) # 3 posts in each page
-#     page = request.GET.get('page')
-#     try:
-#         posts = paginator.page(page)
-#     except PageNotAnInteger:
-#     # If page is not an integer deliver the first page
-#         posts = paginator.page(1)
-#     except EmptyPage:
-#     # If page is out of range deliver last page of results
-#         posts = paginator.page(paginator.num_pages)
-#     return render(request,'blog/post/list.html',{'page': page,'posts': posts})
 
 
 def post_detail(request, year, month, day, post):
@@ -30,5 +18,6 @@ def post_detail(request, year, month, day, post):
                                               publish__month=month,
                                               publish__day=day)
     return render(request,'blog/post/detail.html',{'post': post})
+
 
 
